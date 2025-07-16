9d027209fa1e875e2fa93178cd37806622f6b93c
sadanand <elfstonesadanand@gmail.com>
2025-07-16 10:08:53 -0400

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import {Poppins} from 'next/font/google'
+
+
+const font = Poppins({
+  weight: ['100','200', '300', '400', '500', '600', '700', '800','900'],
+  subsets: ["latin"]
+})
+export const metadata: Metadata = {
+  title: "Travel For You - NextJS 15",
+  description: "Travel Landing Page using NextJS 15",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${font.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
